98f3cdb334b9dc4de0e441f03474f74f025aa1c3
devansien <devansien@gmail.com>
2019-04-27 01:33:23 +1200

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Spyder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        async static Task MainAsync(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Spyder
+
+This project consists of:
+* Alexa media player skill template (audio & video)
+
+## Steps to configure Stream Cast:
+
+```
+    1. Sample step
+```
+
+```
+    2. Sample step
+```
+
+```
+    3. Sample step
+```
