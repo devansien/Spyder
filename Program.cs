29077fd47cd389117f60c807b6e4116bdadaefbd
devansien <devansien@gmail.com>
2019-04-27 02:07:25 +1200

    getting target text confirmed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Spyder
@@ -12,7 +15,27 @@ namespace Spyder
 
         async static Task MainAsync(string[] args)
         {
-            
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync("https://endic.naver.com/search_example.nhn?sLn=en&examType=example&query=b&pageNo=2&ui=lite");
+            string pageContents = await response.Content.ReadAsStringAsync();
+            HtmlDocument pageDocument = new HtmlDocument();
+            pageDocument.LoadHtml(pageContents);
+
+            string targetText = pageDocument.DocumentNode.SelectSingleNode("(//ul[contains(@class, 'list_a list_a_mar')]//input[contains(@type, 'hidden')])").GetAttributeValue("value", "");
+
+
+
+
+
+
+            //using (MemoryStream memoryStream = new MemoryStream(targetText.))
+            //using (FileStream fileStream = new FileStream("b2.txt", FileMode.Create, FileAccess.Write))
+            //{
+            //    memoryStream.CopyTo(fileStream);
+            //}
+
+            Console.WriteLine(targetText);
+            Console.ReadLine();
         }
     }
 }
